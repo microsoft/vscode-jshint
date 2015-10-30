9ec78cf6e80a02c48883c1d41796f56e443b21ee
Dirk Baeumer <dirkb@microsoft.com>
2015-10-30 15:46:54 +0100

    Adopt API

--- jshint/extension.ts
+++ jshint/extension.ts
@@ -1,23 +1,25 @@
 import * as path from 'path';
-import { window, workspace, commands } from 'vscode';
-import { LanguageClient, ClientOptions, ClientStarter, RequestType } from 'vscode-languageclient';
+import { workspace, Disposable } from 'vscode';
+import { LanguageClient, LanguageClientOptions, SettingMonitor, ServerOptions, RequestType } from 'vscode-languageclient';
 
-export function activate() {
+export function activate(subscriptions: Disposable[]) {
 
 	// We need to go one level up since an extension compile the js code into
 	// the output folder.
-	let module = path.join(__dirname, '..', 'server', 'server.js');
+	let serverModule = path.join(__dirname, '..', 'server', 'server.js');
 	let debugOptions = { execArgv: ["--nolazy", "--debug=6004"] };
-	let clientOptions: ClientOptions = {
-		server: {
-			run: { module },
-			debug: { module, options: debugOptions}
-		},
+	let serverOptions: ServerOptions = {
+		run: { module: serverModule },
+		debug: { module: serverModule, options: debugOptions}
+	};
+	let clientOptions: LanguageClientOptions = {
 		languageSelector: ['javascript', 'javascriptreact'],
-		configuration: 'jshint',
-		fileWatchers: workspace.createFileSystemWatcher('**/.jshintrc')
+		synchronize: {
+			configurationSection: 'jshint',
+			fileEvents: workspace.createFileSystemWatcher('**/.jshintrc')
+		}
 	}
 
-	let client = new LanguageClient('JSHint Linter', clientOptions);
-	new ClientStarter(client).watchSetting('jshint.enable');
+	let client = new LanguageClient('JSHint Linter', serverOptions, clientOptions);
+	subscriptions.push(new SettingMonitor(client, 'jshint.enable').start());
 }
