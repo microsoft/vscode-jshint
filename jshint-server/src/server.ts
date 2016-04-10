cdc613e7f946e0300cf247aff445f89f17e36be4
Brian Green <briangreenery@gmail.com>
2016-04-10 15:52:54 -0700

    Add support for "extends". Fixes #13.

--- jshint-server/src/server.ts
+++ jshint-server/src/server.ts
@@ -164,14 +164,53 @@ class OptionsResolver {
 			return result;
 		};
 
-		function readJsonFile(file: string): any {
+		function readJsonFile(file: string, extendedFrom?: string): any {
 			try {
 				return JSON.parse(stripComments(fs.readFileSync(file).toString()));
 			}
 			catch (err) {
-				that.connection.window.showErrorMessage(`Can't load JSHint configuration from file ${file}. Please check the file for syntax errors.`);
+				let location = extendedFrom ? `${file} extended from ${extendedFrom}` : file;
+				that.connection.window.showErrorMessage(`Can't load JSHint configuration from file ${location}. Please check the file for syntax errors.`);
 				return {};
 			}
+		}
+
+		function isArray(value: any): boolean {
+			return Array.isArray(value);
+		}
+
+		function isPlainObject(value: any): boolean {
+			return (typeof value === 'object') && !isArray(value);
+		}
+
+		function merge(parent: any, child: any): any {
+			for (let key in child) {
+				if (isPlainObject(parent[key]) && isPlainObject(child[key])) {
+					merge(parent[key], child[key]);
+				} else if (isArray(parent[key]) && isArray(child[key])) {
+					parent[key] = parent[key].concat(child[key]);
+				} else {
+					parent[key] = child[key];
+				}
+			}
+
+			return parent;
+		}
+
+		function readJSHintFile(file: string, extendedFrom?: string): any {
+			let content = readJsonFile(file, extendedFrom);
+			if (content.extends) {
+				let baseFile = path.resolve(path.dirname(file), content.extends);
+
+				if (fs.existsSync(baseFile)) {
+					content = merge(readJSHintFile(baseFile, file), content);
+				} else {
+					that.connection.window.showErrorMessage(`Can't find JSHint file ${baseFile} extended from ${file}`);
+				}
+
+				delete content.extends;
+			}
+			return content;
 		}
 
 		function isWindows(): boolean {
@@ -198,7 +237,7 @@ class OptionsResolver {
 
 			let configFile = locateFile(fsPath, JSHINTRC);
 			if (configFile) {
-				return readJsonFile(configFile);
+				return readJSHintFile(configFile);
 			}
 		}
 
@@ -206,7 +245,7 @@ class OptionsResolver {
 		if (home) {
 			let file = path.join(home, JSHINTRC);
 			if (fs.existsSync(file)) {
-				return readJsonFile(file);
+				return readJSHintFile(file);
 			}
 		}
 		return jshintOptions;
